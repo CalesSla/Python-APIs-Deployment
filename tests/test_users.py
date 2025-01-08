76eae516be0079b1aeb2636a0dd8c3649e783e1f
Slava Calestru <veaceslavcalestru@gmail.com>
2025-01-08 14:19:17 +0000

    fixture separation file

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,28 @@
-from fastapi.testclient import TestClient
-from app.main import app
+from app import schemas
+from .database import client, session
 
-client = TestClient(app)
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     assert res.json().get("message") == "Hello World"
     assert res.status_code == 200
 
+
+def test_create_user(client):
+    res = client.post("/users/", 
+                      json={"email": "hello1234@gmail.com",
+                            "password": "password123"}
+                      )
+    
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello1234@gmail.com"
+    assert res.status_code == 201
+    
+
+def test_login_user(client):
+    
+    res = client.post("/login", 
+                      data={"username": "hello1234@gmail.com",
+                            "password": "password123"}
+                      )
+    print(res.json())
+    assert res.status_code == 200
